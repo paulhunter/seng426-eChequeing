6ee4e143669128950c2b17e5679285ba9397ffd0
Justin Guze <jaguze@gmail.com>
2014-07-01 17:03:00 -0700

    Added RSAGenerator tests

---
+++ The e-Cheque Client/test/eCheque/RSAGeneratorTests.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eCheque;
+
+import java.security.KeyPair;
+import java.security.NoSuchAlgorithmException;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author jaguz_000
+ */
+public class RSAGeneratorTests {
+    
+    RSAGenerator rsaGenerator;
+    
+    public RSAGeneratorTests() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        rsaGenerator = new RSAGenerator();
+    }
+    
+    @After
+    public void tearDown() {
+        rsaGenerator = null;
+    }
+
+    /*
+     * Test the RSAGenerator. Ensure that the generator creates
+     * different key pairs every time, and that they are never null.
+     */
+    @Test
+    public void testRSAGenerator() throws NoSuchAlgorithmException {
+        KeyPair rsaKeyPair1 = rsaGenerator.GenerateRSAKeys();
+        KeyPair rsaKeyPair2 = rsaGenerator.GenerateRSAKeys();
+        assertThat(rsaKeyPair1, not(nullValue()));
+        assertThat(rsaKeyPair2, not(nullValue()));
+        
+        assertThat(rsaKeyPair1.getPublic().getEncoded(), not(equalTo(rsaKeyPair2.getPublic().getEncoded())));
+        assertThat(rsaKeyPair1.getPrivate().getEncoded(), not(equalTo(rsaKeyPair2.getPrivate().getEncoded())));        
+    }
+}
